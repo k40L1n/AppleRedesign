896ad28bce69dbaa3c925657477b38eef3e6f791
k40L1n <droidaarqam@gmail.com>
2022-12-14 23:31:58 +0530

    added icons to sanity CMS backend

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -6,7 +6,7 @@ function Landing() {
   return (
     <section className='sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8'>
       <div className='space-y-8'>
-        <h1 className='text-5-xl space-y-3 font-semibold tracking-wide lg:text-6xl xl:text-7xl'>
+        <h1 className='space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl'>
           <span className='block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
             Powered
           </span>
@@ -20,6 +20,7 @@ function Landing() {
           </a>
         </div>
       </div>
+
       <div className='relative hidden h-[450px] w-[450px] transition duration-500 md:inline lg:h-[650px] lg:w-[650px]'>
         <Image
           alt='image missing'
